cc5d1cfcc8e4c7e2da1b6d0842bbf565d3c335ba
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2024-08-06 21:30:53 +0200

    update .gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { Button } from '@/components/ui/button';
+import prisma from '@/db/prisma';
+import { User } from '@prisma/client';
 
-export default function Home() {
+export default async function Home() {
+  const users: User[] = await prisma.user.findMany();
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Button>Click me</Button>
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {users ? (
+            users.map((user) => <li key={user.id}>{user.name}</li>)
+          ) : (
+            <li>db not working correctly</li>
+          )}
+        </ul>
+      </div>
     </main>
   );
 }
